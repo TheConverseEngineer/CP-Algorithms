f058b070d01c4d641265751da1ef817c072748ff
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2022-07-04 10:16:17 -0400

    Create SimpleSegmentTree.cpp

---
+++ SegmentTrees/SimpleSegmentTree.cpp
@@ -0,0 +1,32 @@
+/** Implementation of a simple segment tree that supports adition by default
+  * To change the function, replace the following:
+      - the plus sign on line 15
+      - the plus sign on line 19
+      - the += signs on lines 25 and 26
+  * NOTE: query range is inclusive-exclusive: ie. [a, b)
+*/
+struct SegmentTree {
+	int n; 
+	vector<int> segTree;
+	SegmentTree(int _n) {
+		n = _n;
+		segTree.assign(2*n, 0);
+	}
+	SegmentTree(const vector<int>& values) {
+		n = values.size();
+		segTree.assign(2*n, 0);
+		for (int i{n}; i < 2*n; i++) segTree[i] = values[i-n];
+  		for (int i{n - 1}; i > 0; i--) segTree[i] = segTree[i<<1] + segTree[i<<1|1];
+	}
+	void setValue(int pos, int value) { 
+  		for (segTree[pos += n] = value; pos > 1; pos >>= 1) segTree[pos>>1] = segTree[pos] + segTree[pos^1];
+	}
+	int query(int l, int r) {
+  		int ret = 0;
+  		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    		if (l&1) ret += segTree[l++];
+    		if (r&1) ret += segTree[--r];
+  		}
+  		return ret;
+	}
+};
