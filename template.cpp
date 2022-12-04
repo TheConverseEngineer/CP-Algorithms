45f95e9d35850e0acacc2c3c9beeabcb4bd24930
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2022-12-04 16:58:03 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T, null_type, less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+#define FOR(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define sqr(x) ((ll)(x))*(x)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	//freopen("fcolor.in", "r", stdin);
+	//freopen("fcolor.out", "w", stdout);
+}
